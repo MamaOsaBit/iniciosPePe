8be790ae4ffd33cde69146ca5ed69976d4c1153b
ramosperedo <andrewmcpray@gmail.com>
2020-05-28 22:38:00 -0300

    fix

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+ probando
+    
+Hola, esto es un texto de prueba.
+</head>
+<body>
+<?php
+    echo "kha"
+    ?>
+        <h1>
+        botones <?php funcion()?>
+        </h1>
+    <?php
+    echo "pp"
+    ?>
+
+</body>
+</html>

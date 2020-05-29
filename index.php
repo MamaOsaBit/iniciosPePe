818a8b8d3c37b2fb45b32f8a45c8759e2a54ac43
MamaOsaBit <jose.varas13@gmail.com>
2020-05-28 21:38:20 -0400

    Fix

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+
+</head>
+<body>
+<?php
+    echo "kha"
+    ?>
+        <h1>
+        botones <?php funcion()?>
+        </h1>
+    <?php
+    echo "pp"
+    ?>
+
+
+asdasdasd
+Ok, este es otro texto de prueba
+
+</body>
+</html>
